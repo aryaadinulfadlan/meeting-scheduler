226e2207b1c0c2b8669448d8028c4d5521d589a2
Arya Adinul Fadlan <adinul.fadlan04@gmail.com>
2025-11-17 09:09:06 +0700

    submitting username to search organizer

--- components/username-checker.tsx
+++ components/username-checker.tsx
@@ -1,24 +1,31 @@
 "use client";
 
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
-import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function UsernameChecker() {
   const [text, setText] = useState("");
+  const { push } = useRouter();
   const { origin } = window.location;
+  function handleSubmit(e: FormEvent) {
+    e.preventDefault();
+    push(`${origin}/${text}`);
+  }
   return (
-    <div className="text-sm lg:text-base grid gap-2 mt-1">
+    <div className="text-sm lg:text-base mt-1 grid gap-3">
       Or you can check the username here to see if they have a schedule.
-      <Input
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Username"
-      />
-      <Button size="icon-sm" asChild className="font-semibold">
-        <Link href={`${origin}/${text}`}>Check</Link>
-      </Button>
+      <form onSubmit={handleSubmit} className="grid gap-2">
+        <Input
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Username"
+        />
+        <Button type="submit" size="icon-sm" className="font-semibold">
+          Check
+        </Button>
+      </form>
     </div>
   );
 }
